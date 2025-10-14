5c4a5f8bcce0168cdad4e80a442e815503f35e69
ragztigadi <raghavtigadi22@gmail.com>
2025-10-14 01:02:18 -0400

    pipeline runnig successfully

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -1,15 +1,23 @@
-    from praw import Reddit
-    import praw
-    import sys
+from praw import Reddit
+import praw
+import sys
 
-    def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
-        try:
-            reddit = praw.Reddit(
-                client_id = client_id,
-                client_secret= client_secret,
-                user_agent= user_agent)
-            print("Connected to Reddit!")
-            return reddit
-        except Exception as e:
-            print(e)
-            sys.exit(1)
+def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
+    try:
+        reddit = praw.Reddit(
+            client_id = client_id,
+            client_secret= client_secret,
+            user_agent= user_agent)
+        print("Connected to Reddit!")
+        return reddit
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter:str, limit = None):
+    subreddit = reddit_instance.subreddit(subreddit)
+    posts = subreddit.top(time_filter=time_filter, limit=limit)
+
+    # post_lists = []
+    print(posts)
+    # for post in post_lists:
--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,9 +1,10 @@
 from etls.reddit_etl import connect_reddit
 from utils.constants import SECRET, CLIENT_ID
+from etls.reddit_etl import extract_posts   
 
 def reddit_pipeline(file_name: str, subreddit: str,time_filter = 'day', limit=None):
     # Connecting to reddit instance
     instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
 
     #Extraction
-    posts = extract_posts(instace, subreddit, time_filter, limit)
+    posts = extract_posts(instance, subreddit, time_filter, limit)
