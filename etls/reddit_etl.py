cdf3b5df235ed497c16a242ee5f12bdda9144351
ragztigadi <raghavtigadi22@gmail.com>
2025-10-17 01:11:16 -0400

    etl is done data converted into the csv as a result:

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -1,7 +1,13 @@
-from praw import Reddit
+import pandas as pd
+import numpy as np
 import praw
 import sys
 
+from praw import Reddit
+from utils.constants import POST_FIELDS
+
+
+# I - API connection
 def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     try:
         reddit = praw.Reddit(
@@ -14,10 +20,35 @@ def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
         print(e)
         sys.exit(1)
 
+# II - Data Extraction 
 def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter:str, limit = None):
     subreddit = reddit_instance.subreddit(subreddit)
     posts = subreddit.top(time_filter=time_filter, limit=limit)
 
-    # post_lists = []
-    print(posts)
-    # for post in post_lists:
+    post_lists = []
+    
+    for post in posts:
+        post_dict = vars(post) 
+        print(posts)
+        post = {key: post_dict[key] for key in POST_FIELDS}
+        post_lists.append(post)
+    return post_lists
+
+# III - Data Transformation
+def transform_data(post_df: pd.DataFrame):
+    post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
+    post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
+    post_df['author'] = post_df['author'].astype(str)
+    edited_mode = post_df['edited'].mode()
+    post_df['edited'] = np.where(post_df['edited'].isin([True, False]),
+                                 post_df['edited'], edited_mode).astype(bool)
+    post_df['num_comments'] = post_df['num_comments'].astype(int)
+    post_df['score'] = post_df['score'].astype(int)
+    post_df['title'] = post_df['title'].astype(str)
+
+    return post_df
+
+# IV - Data Load
+def load_data_to_csv(data: pd.DataFrame, path: str):
+    data.to_csv(path, index=False)
+    return data
